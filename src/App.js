48c0694b5580975702a6ec466bf28e2711f7214c
k1p1w <k1p1wzbs@gmail.com>
2022-08-07 01:46:33 +0300

    react-sneakers

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const array = [
+  {
+    title: "Men's sneakers Nike Blazer Mid Suede",
+    price: 7700,
+    imageUrl: process.env.PUBLIC_URL + '/img/sneakers/1.jpg',
+  },
+  {
+    title: "Men's sneakers Nike Air Max 270",
+    price: 8400,
+    imageUrl: process.env.PUBLIC_URL + '/img/sneakers/2.jpg',
+  },
+  {
+    title: "Men's sneakers Nike Blazer Mid Suede",
+    price: 7700,
+    imageUrl: process.env.PUBLIC_URL + '/img/sneakers/3.jpg',
+  },
+  {
+    title: 'Sneakers Puma X Aka Boku Future Rider',
+    price: 5200,
+    imageUrl: process.env.PUBLIC_URL + '/img/sneakers/4.jpg',
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
       <Drawer />
-
       <Header />
-
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>All sneakers</h1>
@@ -20,12 +41,15 @@ function App() {
             <input placeholder="Search...." />
           </div>
         </div>
-
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {array.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,19 @@
-function Card() {
+import styles from './Card.module.scss';
+
+function Card(props) {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src={process.env.PUBLIC_URL + '/img/unliked.svg'} alt="Unliked" />
       </div>
-      <img
-        width={133}
-        height={112}
-        src={process.env.PUBLIC_URL + '/img/sneakers/1.jpg'}
-        alt="Sneakers"
-      />
-      <h5>Men's sneakers Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Price:</span>
-          <b> 7 700 uah</b>
+          <b>{props.price} uah</b>
         </div>
-        <button className="button">
+        <button className="button" onClick={props.onClick}>
           <img
             width={11}
             height={11}
